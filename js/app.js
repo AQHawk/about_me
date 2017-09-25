51f40e0492078e6d8b60af98073a3e6d6ff30e04
AQHawk <ironhawkcreations@gmail.com>
2017-09-24 23:45:42 -0700

    Fixed JS

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 
 var userName = prompt('Hi! What\'s your name?');
-alert('Hi, ' + userName + '. After reading my about me, let\'s see if you can answer some questions. Sound Good?');
+alert('Hi, ' + userName + '. Up for a little guessing game?, let\'s see if you can answer some questions about me. Sound Good?');
 
 var answer1 = prompt('Was I born and raised in Washington? Y/N').toLowerCase();
 console.log('Answer 1 is ' + answer1);
@@ -18,7 +18,7 @@ console.log('Answer 2 is ' + answer2);
 if (answer2 ==='y' || answer2 === 'yes') {
   alert('Correctomundo!..and I have a million stories to tell about it');
 } else {
-  alert('You are incorrect. I left the service in 2009 after 20 years.');
+  alert('You are incorrect. I left the service in 2009 after a full career.');
 }
 
 var answer3 = prompt('Was I a bartender? Y/N').toLowerCase();
@@ -26,7 +26,7 @@ console.log('Answer 3 is ' + answer3);
 if (answer3 ==='y' || answer3 === 'yes') {
   alert('You are correct...and the ladies loved our "Cocktail" routine');
 } else {
-  alert('You are incorrect. I learned the ropes at the Salina Country Club for the first year then tended bar for another three years');
+  alert('You are incorrect. I learned the ropes at the Salina Country Club for the first year, then tended bar for another three years');
 }
 
 var answer4 = prompt('Did I like my work as an IT?').toLowerCase();
@@ -37,59 +37,78 @@ if (answer4 ==='n' || answer4 === 'no') {
   alert('You are incorrect');
 }
 
-var answer5 = prompt('Do I like kitties?').toLowerCase();
+var answer5 = prompt('Do I like kittens?').toLowerCase();
 console.log('Answer 5 is ' + answer5);
 if (answer5 ==='y' || answer5 === 'yes') {
-  alert('You are correct! That one was kind of a gimme. Who doesn\'t like kitties!?');
+  alert('You are correct! That one was kind of a gimme. Who doesn\'t like kittens!?');
 } else {
-  alert('Of course I do. Who doesn\'t like kitties!?');
+  alert('Of course I do. Who doesn\'t like kittens!?');
 }
 
-var guesses = 3;
+
+var guessesRemaining = 4;
+var cupsCoffee = 8;
+
+while (guessesRemaining > 0) {
 
 var answer6 = parseInt(prompt('Now we\'re going to try a little guessing game if you\'re up for it. Can you tell me how many cups of coffee I\'ve had this week already? You get 4 guesses.'));
-while (guesses > 0 ) {
-  if (answer6 === 7) {
-  alert('You are correct! I am dosin on Coffee!');
-  break;
-} else {
-  alert('Sorry. Try again.');
-  guesses--;
-  answer6 = parseInt(prompt('How many?'));
+    if (answer6.toString() == 'NaN') {
+      alert('Sorry, this needs to be a number');
+      // answer6 = prompt('beexlebub');
+    } else if(answer6 === cupsCoffee) {
+      alert('You are correct! I am dosin on Coffee!');
+      break;
+      // answer = prompt('beexl');
+    } else if(answer6 < cupsCoffee) {
+      alert('Too low!Try again');
+      guessesRemaining--;
+      // answer6 = prompt('too low');
+    } else {
+      alert('Too high! Try again');
+      guessesRemaining--;
+    }
+if (guessesRemaining === 0){
+  alert('You\'re all out of guesses. 8 was the magic number')
 }
 }
-console.log('Answer 6 is ' + answer6);
 
+// -----------------------------------------------------
+// Below is another way to do it
 
-
-// I had to comment out question 7 for the rest to work. Struggled with it. I'll have to resubmit.
-
-// var answer7 = (prompt('Let's see if you\'re better at this. Can you guess a state that I have lived in besides Washington?');
+// 'use strict';
 //
-// var states;
-// states = ['California', 'Oregon', 'Colorado', 'Texas', 'Missouri', 'Virginia', 'South Carolina'];
+// var guessesRemaining = 4;
+// var notFinished = true;
+// var cupsCoffee = 8;
 //
-// for (var i=0; i < myArray.length; i++) {
-//   console.log(myArray[i]);
-}
-
-
-
-
-
-
-
-
-/*
-Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical. Or not. It's up to you! It's your project!
-
-The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
-
-Useful and descriptive console.log() messages in the JS are well written and correctly displaying to the browser console for each question of the guessing game.
-
-Use CSS to style your page as you see fit, to the level of abilities in CSS that you currently have
-
-Add, Commit, Push process is being followed; it is evident in GitHub that commits are made regularly, and with good commit messages that explain the WHY of the commit. */
-
-// OR ||
-// AND &&
+// var answer6 = parseInt(prompt('Now were going to try a little guessing game if youre up for it. Can you tell me how many cups of coffee Ive had this week already? You get 4 guesses.'));
+// console.log(answer6);
+// while (guessesRemaining > 0 && notFinished) {
+//
+//   if (isNaN(answer6)) {
+//     console.log(answer6);
+//     alert('Sorry, this needs to be a number');
+//     guessesRemaining++;
+//   }
+//
+//   if (guessesRemaining !== 4) {
+//     answer6 = parseInt(prompt('Enter another number.'));
+//     console.log(answer6);
+//   }
+//
+//   if (answer6 === cupsCoffee) {
+//     alert('You are correct! I am dosin on Coffee!');
+//     console.log(answer6);
+//     notFinished = false;
+//
+//   } else if(answer6 < cupsCoffee) {
+//     alert('Too low!Try again');
+//     // answer6 = prompt('too low');
+//
+//   } else {
+//     console.log(answer6);
+//     alert('Too high! Try again');
+//   }
+//   guessesRemaining--;
+// }
+// -------------------------------------------------------------
